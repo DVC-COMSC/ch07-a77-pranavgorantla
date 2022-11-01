5efa5ebee7dec811b028b11fc783b9e01ed4afff
pranavgorantla <111912611+pranavgorantla@users.noreply.github.com>
2022-11-01 12:40:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
 
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
+print(f"The original list 1 : {list1}")
+print(f"The original list 2 : {list2}")
 
-# ******************************
-# Make your Code
-# ******************************
+temp = list(zip(list1, list2))
+random.shuffle(temp)
+res1, res2 = zip(*temp)
 
-# print (num3) 
+res1, res2 = list(res1), list(res2)
+ 
+print(f"List 1 after shuffle :  {res1}")
+print(f"List 2 after shuffle :  {res2}")
